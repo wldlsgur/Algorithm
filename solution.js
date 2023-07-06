ff7bc3d38236ed48cb52ff63fec57b793341b6d0
wldlsgur <dhgg321@naver.com>
2023-07-06 18:28:25 +0900

    백준 10026 적록색약 문제 해결

--- solution.js
+++ solution.js
@@ -1,124 +1,76 @@
 const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 const inputs = [];
+const dy = [-1, 1, 0, 0];
+const dx = [0, 0, -1, 1];
+
+let nonBlindVisited = [];
+let blindVisited = [];
 
 rl.on("line", (input) => {
     inputs.push(input.trim());
 }).on("close", () => {
-    const [V, E] = inputs[0].split(" ").map(Number);
-    const start = Number(inputs[1]);
-    const graphInfo = inputs.slice(2, inputs.length).map(value => value.split(" ").map(Number));
+    const N = Number(inputs[0]);
+    const array = inputs.slice(1, inputs.length).map(value => value.split(""));
 
-    solution(V, E, start, graphInfo);
+    solution(N, array);
 });
 
-function solution(V, E, start, graphInfo) {
-    const list = createList(V);
+function solution(N, array) {
+    const result = [0, 0];
+    nonBlindVisited = Array.from({ length: N }, () => new Array(N).fill(false));
+    blindVisited = Array.from({ length : N }, () => new Array(N).fill(false));
 
-    for(const uvw of graphInfo){
-        const [u, v, w] = uvw; // u : 시작점, v : 도착점, w : 가중치
-        
-        list[u].push([v, w]);
-    }
-    
-    const distance = dijkstra(list, start, V);
-
-    distance.slice(1).map(value => value === Infinity ? console.log("INF") : console.log(value));
-}
-
-function dijkstra(list, start, V) {
-    const priorityQueue = new MinHeap();
-    const visited = Array.from({ length: V + 1 }, () => false);
-    const distance = Array.from({ length: V + 1 }, () => Infinity);
-
-    distance[start] = 0; // 자기 자신 거리 0
-    priorityQueue.insert([start, 0]);
-
-    while(!priorityQueue.isEmpty()){
-        const [vertex, weight] = priorityQueue.getMinWeight();
-
-        if (visited[vertex] || !vertex) continue;
-        else visited[vertex] = true;
-
-        for(let i=0 ; i<list[vertex].length ; i++) {
-            const [arrive, weight] = list[vertex][i];
-
-            if(distance[arrive] > distance[vertex] + weight) {
-              distance[arrive] = distance[vertex] + weight;
-              priorityQueue.insert([arrive, distance[arrive]]);
+    for(let i=0; i<N ; i++){
+        for(let j=0; j<N ; j++){
+            if(!nonBlindVisited[i][j]) {
+                result[0]++;
+                bfs(i, j, array, N, false);
+            }
+            if(!blindVisited[i][j]) {
+                result[1]++;
+                bfs(i, j, array, N, true);
             }
         }
     }
-    return distance;
+    console.log(result[0], result[1]);
 }
 
-function createList(V) {
-    const list = {};
+function bfs(i, j, array, N, isBlind) {
+    const color = array[i][j];
+    const queue = [];
 
-    for(let i=1 ; i<=V ; i++) list[i] = [];
+    isBlind ? blindVisited[i][j] = true : nonBlindVisited[i][j] = true;
+    queue.push([i, j]);
 
-    return list;
-}
+    while(queue.length > 0) {
+        const [y, x] = queue.shift();
 
-class MinHeap {
-    heap = [];
+        for(let z=0; z<4 ; z++) {
+            const newY = y + dy[z];
+            const newX = x + dx[z];
 
-    constructor() { this.heap.push([null]); }
+            if(newY < 0 || newY >= N || newX < 0 || newX >= N) continue;
+            
+            // 적록색약이 아닐때
+            if(!isBlind && !nonBlindVisited[newY][newX] && array[newY][newX] === color) {
+                nonBlindVisited[newY][newX] = true;
+                queue.push([newY, newX]);
+                continue;
+            }
 
-    getParentIndex(index) { return Math.floor(index / 2); }
-    getLeftChildIndex(index) { return index * 2; }
-    getRightChildIndex(index) { return index * 2 + 1; }
+            let blindColor = color;
 
-    isEmpty() { return this.heap[1] ? false : true; }
+            if (color === "R") blindColor = "G";
+            if (color === "G") blindColor = "R";
 
-    swap(index1, index2) {
-      let temp = this.heap[index1];
-      this.heap[index1] = this.heap[index2];
-      this.heap[index2] = temp;
-    }
-
-    insert(value) {
-      this.heap.push(value);
-
-      let currentIndex = this.heap.length - 1;
-
-      while (currentIndex > 1 && this.heap[currentIndex][1] < this.heap[this.getParentIndex(currentIndex)][1]) {
-        this.swap(currentIndex, this.getParentIndex(currentIndex));
-        currentIndex = this.getParentIndex(currentIndex);
-      }
-    }
-
-  getMinWeight() {
-      if (this.isEmpty()) return [null, null];
-
-      const [vertex, weight] = this.heap[1];
-      let currentIndex = 1;
-
-      this.heap[1] = this.heap.pop();
-      if (this.heap.length === 2) {
-        this.heap.pop();
-        return [vertex, weight];
-      }
-      while (true) {
-        const leftChildIndex = this.getLeftChildIndex(currentIndex);
-        
-        if (leftChildIndex >= this.heap.length) break;
-
-        const rightChildIndex = this.getRightChildIndex(currentIndex);
-        let smallerChildIndex;
-
-        if (rightChildIndex < this.heap.length) {
-          smallerChildIndex = this.heap[leftChildIndex][1] < this.heap[rightChildIndex][1] ? leftChildIndex : rightChildIndex;
-        } 
-        else {
-          smallerChildIndex = leftChildIndex;
+            // 적록색약인 경우
+            if(isBlind && !blindVisited[newY][newX]) {
+                if(array[newY][newX] === color || array[newY][newX] === blindColor) {
+                    blindVisited[newY][newX] = true;
+                    queue.push([newY, newX]);
+                }
+            }
         }
-
-        if (this.heap[currentIndex][1] <= this.heap[smallerChildIndex][1]) break;
-
-        this.swap(currentIndex, smallerChildIndex);
-        currentIndex = smallerChildIndex;
-      }
-      return [vertex, weight];
     }
 }
