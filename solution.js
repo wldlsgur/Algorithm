1d430fe45c77f8bde1236cfba4af3d35423f9b0b
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-08-11 00:49:03 +0900

    프로그래머스 LV1 공원 산책 해결 완료

--- solution.js
+++ solution.js
@@ -1,45 +1,44 @@
 function solution(park, routes) {
     const yLength = park.length;
     const xLength = park[0].length;
-    const startPoitn = [];
+    let startPoitn ;
 
     for(let i=0 ; i<yLength ; i++) {
         if(park[i].includes("S")) {
-            startPoitn.push([i, park[i].indexOf("S")]);
+            startPoitn = [i, park[i].indexOf("S")];
             break;
         }
     };
 
-    for(let i=0 ; i<routes.length ; i++) {
-        const [y, x] = startPoitn[0];
+    outLoof : for(let i=0 ; i<routes.length ; i++) {
+        const [y, x] = startPoitn;
         const [direction, distance] = routes[i].split(" ");
         let newX = x;
         let newY = y;
 
-        switch(direction) {
-            case "E" :
-                newX += Number(distance);;
-                break;
-            case "W" :
-                newX -= Number(distance);;
-                break;
-            case "N" :
-                newY -= Number(distance);
-                break;
-            case "S" :
-                newY += Number(distance);
-                break;
+        for(let j=0 ; j<Number(distance) ; j++) {
+            switch(direction) {
+                case "E" :
+                    newX++;
+                    break;
+                case "W" :
+                    newX--
+                    break;
+                case "N" :
+                    newY--;
+                    break;
+                case "S" :
+                    newY++;
+                    break;
+            }
+            if(newX < 0 || newX >= xLength || newY < 0 || newY >= yLength) continue outLoof;
+            if(park[newY][newX] === "X") continue outLoof;
         }
-
-        if(newX < 0 || newX >= xLength || newY < 0 || newY >= yLength) continue;
-        if(park[newY][newX] === "X") continue;
-
-        startPoitn[0] = [newY, newX];
+        startPoitn = [newY, newX];
     }
-    console.log(startPoitn[0])
-    return startPoitn[0];
+    return startPoitn;
 }
 
 // solution(["SOO", "OOO", "OOO"], ["E 2", "S 2", "W 1"]); // [2, 1]
-solution(["SOO","OXX","OOO"],	["E 2","S 2","W 1"]); // [0, 1]
+// solution(["SOO","OXX","OOO"],	["E 2","S 2","W 1"]); // [0, 1]
 // solution(["OSO","OOO","OXO","OOO"],	["E 2","S 3","W 1"]) // [0, 0]
