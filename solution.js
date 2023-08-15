5772d072ef2db15044238d170045b10e3e703e66
wldlsgur <dhgg321@naver.com>
2023-08-15 18:58:47 +0900

    백준 1520번 내리막길 시간초과 발생 (미해결)

--- solution.js
+++ solution.js
@@ -1,58 +1,50 @@
-function solution(name, yearning, photo) {
-    const scores = {};
-    const result = [];
-    
-    for(let i=0 ; i<name.length ; i++) {
-        const charater = name[i];
-        const score = yearning[i];
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+const inputs = [];
+const dy = [-1, 1, 0, 0];
+const dx = [0, 0, -1, 1];
+let result = 0;
 
-        scores[charater] = score;
-    }
+rl.on("line", (input) => {
+  inputs.push(input.trim());
+}).on("close" , () => {
+  const [M, N] = inputs[0].split(" ").map(Number);
+  const heights = inputs.slice(1).map(value => value.split(" ").map(Number));
 
-    for(let i=0 ; i<photo.length ; i++) {
-        let totalScore = 0;
+  solution(M, N, heights);
+})
 
-        for(let j=0; j<photo[i].length ; j++) {
-            const charater = photo[i][j];
-            const score = scores[charater];
+function solution(M, N, heights) {
+  // 방문 여부 확인 2차원 배열
+  const visited = Array.from({length : M}, () => new Array(N).fill(false));
 
-            if(score) {
-                totalScore += score;
-            }
-        }
-        result.push(totalScore);
-    }
+  dfs(M, N, heights, visited, [0, 0]);
 
-    return result;
+  console.log(result);
 }
 
-// console.log(solution(
-//     ["may", "kein", "kain", "radi"],
-//     [5, 10, 1, 3],
-//     [
-//       ["may", "kein", "kain", "radi"],
-//       ["may", "kein", "brin", "deny"],
-//       ["kon", "kain", "may", "coni"]
-//     ]
-//   )
-// ) // [19, 15, 6]
-// console.log(solution(
-//     ["kali", "mari", "don"],
-//     [(11, 1, 55)],
-//     [
-//       ["kali", "mari", "don"],
-//       ["pony", "tom", "teddy"],
-//       ["con", "mona", "don"]
-//     ]
-//   )
-// ) // [67, 0, 55]
-// console.log(solution(
-//     ["may", "kein", "kain", "radi"],
-//     [5, 10, 1, 3)],
-//     [
-//       ["may"], 
-//       ["kein", "deny", "may"], 
-//       ["kon", "coni"]
-//     ]
-//   )
-// ) // [5, 15, 0];
+function dfs(M, N, heights, visited, start) {
+  const [y, x] = start;
+
+  // 만약 현재 방문 중인 좌표가 배열의 오른쪽 맨 아래라면 길 1개 증가 후 현재 함수 종료
+  if(y === M - 1 && x === N - 1) return result++;
+
+  visited[y][x] = true;
+  
+  // 상 하 좌 우 탐색
+  for(let i=0; i<4 ; i++) {
+    const currentNumber = heights[y][x];
+    const newY = y + dy[i];
+    const newX = x + dx[i];
+
+    // 배열의 범위 벗어나면 생략
+    if(newY < 0 || newY >= M || newX < 0 || newX >= N) continue;
+
+    // 다음 이동할 곳이 현재보다 더 작을때 dfs 탐색
+    if(currentNumber > heights[newY][newX]) {
+      dfs(M, N, heights, visited, [newY, newX]);
+      // 방문 후 돌아오면 다시 방문 처리를 해제해준다.
+      visited[newY][newX] = false;
+    }
+  }
+}
