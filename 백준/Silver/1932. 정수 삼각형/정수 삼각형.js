ca2ccd421cb46cf1f62b794e3d72d6f960128276
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2023-08-22 19:27:02 +0900

    [Silver I] Title: 정수 삼각형, Time: 320 ms, Memory: 14932 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+const inputs = [];
+
+rl.on("line", (input) => {
+  inputs.push(input.trim());
+}).on("close" , () => {
+  const N = Number(inputs.shift());
+  const info = inputs.map(value => value.split(" ").map(Number));
+
+  solution(N, info);
+})
+
+function solution(N, info) {
+  /*
+  7
+  3 8
+  8 1 0
+  2 7 4 4
+  4 5 2 6 5 
+  */
+  // N - 2 배열 부터 위로 0까지 탐색한다.
+  // 아래 배열의 j와 j+1 인덱스를 더한 두 개의 값을 비교하여 더 큰 값을 갱신
+  for(let i=N-2 ; i>=0 ; i--) {
+    for(let j=0 ; j<info[i].length ; j++) {
+      info[i][j] = Math.max(info[i + 1][j] + info[i][j], info[i + 1][j + 1] + info[i][j]);
+    }
+  }
+
+  console.log(info[0][0]);
+}
