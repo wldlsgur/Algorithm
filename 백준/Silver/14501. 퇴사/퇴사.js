54981ce76556cf62885b07867c8b91c8cd6b30ab
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2023-09-04 17:47:58 +0900

    [Silver III] Title: 퇴사, Time: 156 ms, Memory: 9740 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -13,11 +13,28 @@ rl.on("line", (input) => {
 });
 
 function solution(N, info) {
-    dfs(info, N, 0, 0);
+    const dp = new Array(N + 1).fill(0);
 
-    console.log(max);
+    for(let i=N - 1 ; i>=0 ; i--) {
+        const [T, P] = info[i];
+
+        if(i + T <= N) {
+            dp[i] = Math.max(P + dp[i + T], dp[i + 1]); // 참석한 경우 or 참석 안하는 경우 중 max
+        }
+        else {
+            dp[i] = dp[i + 1]; // 참석 못하는 범위면 안하는 경우 max
+        }
+    }
+
+    console.log(dp[0]);
+
+    /*dfs(info, N, 0, 0);
+
+    console.log(max);*/
 }
 
+
+// 완전 탐색 방법
 function dfs(info, N, index, sum) {
     if(index >= N) {
         return max = Math.max(max, sum);
@@ -25,8 +42,10 @@ function dfs(info, N, index, sum) {
 
     const [T, P] = info[index];
 
+    // 상담 하는 경우
     if(index + T <= N) {
         dfs(info, N, index + T, sum + P);
     }
+    // 상담하지 않는 경우
     dfs(info, N, index + 1, sum);
 }
