16a76f1f15c26a4f80e7e318e7ad452de66bd393
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-09-07 16:04:55 +0900

    백준 ACM craft 문재 해결 중

--- solution.js
+++ solution.js
@@ -3,32 +3,63 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 const inputs = [];
 
 rl.on("line", (input) => {
-  inputs.push(input.trim());
+    inputs.push(input.trim());
 }).on("close", () => {
-  const N = Number(inputs.shift());
-  const info = inputs.map((value) => value.split(" ").map(Number));
+    const testCase = Number(inputs.shift());
 
-  solution(N, info);
+    for(let tc=0 ; tc<testCase ; tc++) {
+        const [N, K] = inputs.shift().split(" ").map(Number);
+        const buildTime = inputs.shift().split(" ").map(Number);
+        const info = inputs.splice(0, K).map(value => value.split(" ").map(Number));
+        const W = Number(inputs.shift());
+
+        solution(N, K, W, buildTime, info);
+    }
 });
 
-function solution(N, info) {
-  /*
-  7
-  3 8
-  8 1 0
-  2 7 4 4
-  4 5 2 6 5 
-  */
-  // N - 2 배열 부터 위로 0까지 탐색한다.
-  // 아래 배열의 j와 j+1 인덱스를 더한 두 개의 값을 비교하여 더 큰 값을 갱신
-  for (let i = N - 2; i >= 0; i--) {
-    for (let j = 0; j < info[i].length; j++) {
-      info[i][j] = Math.max(
-        info[i + 1][j] + info[i][j],
-        info[i + 1][j + 1] + info[i][j]
-      );
+function solution(N, M, W, buildTime, info) {
+    const indegree = new Array(N + 1).fill(0);
+    const list = {};
+
+    for(let i=1 ; i<=N ; i++) {
+        list[i] = [];
     }
-  }
-  console.log(info);
-  console.log(info[0][0]);
+
+    for(const [start, arrive] of info) {
+        list[start].push(arrive);
+        indegree[arrive]++;
+    }
+
+    topologtSort(N, list, buildTime, indegree);
+}
+
+function topologtSort(N, list, buildTime, indegree) {
+    const queue = [];
+    const result = new Array(N + 1).fill(0);
+
+    for(let i=1 ; i<=indegree.length ; i++) {
+        if(indegree[i] === 0) {
+            queue.push(i);
+        }
+    }
+
+    while(queue.length > 0) {
+        const node = queue.shift();
+        let maxTime = 0;
+
+        result[maxT]
+
+        // 큐에서 꺼낸 노드랑 연결된 노드를 탐색
+        for(const nextNode of list[node]) {
+            maxTime = Math.max(buildTime[nextNode], maxTime);
+            indegree[nextNode]--; // 연결된 노드의 진입 차수를 삭제
+
+            // 만약 연결된 노드의 진입차수를 삭제후 0이라면 큐에 삽입
+            if(indegree[nextNode] === 0) {
+                queue.push(nextNode);
+            }
+        }
+    }
+
+    return result;
 }
