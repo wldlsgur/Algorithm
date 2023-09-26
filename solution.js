216c82c04b34d75f08938e83cf0418da2decf206
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-09-26 17:08:40 +0900

    큐 정리 완료

--- solution.js
+++ solution.js
@@ -1,19 +1,53 @@
-function counting() {
-    let i;
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+const inputs = [];
 
-    for(i=0 ; i<5 ; i++) {
-        setTimeout(() => {
-            console.log(i);
-        }, i * 100);
+rl.on("line", (input) => {
+    inputs.push(input.trim());
+}).on("close", () => {
+    const N = Number(inputs.shift());
+    const graphInfo = inputs.map(value => value.split(" ").map(Number));
+    
+    solution(N, graphInfo);
+})
+
+function solution(N, graphInfo) {
+    const list = {};
+    let result = 0;
+
+    for(let i=1; i<=N ; i++) {
+        list[i] = [];
     }
+
+    for(const [start, arrive, cost] of graphInfo) {
+        list[start].push([arrive, cost]);
+        list[arrive].push([start, cost]);
+    }
+
+    let targetNode = 1;
+    for(let i=0 ; i<2 ; i++) {
+        const max = { node : 0, cost : 0 };
+        const visited = new Array(N + 1).fill(false);
+
+        dfs(max, visited, list, targetNode, 0);
+        targetNode = max.node;
+        result = max.cost;
+    }
+    
+    console.log(result);
 }
 
-function counting() {
-    for(let i=0 ; i<5 ; i++) {
-        setTimeout(() => {
-            console.log(i);
-        }, i * 100);
+function dfs(max, visited, list, currNode, currCost) {
+    visited[currNode] = true;
+
+    if(max.cost < currCost) {
+        max.node = currNode;
+        max.cost = currCost;
+    }
+
+    for(const [nextNode, cost] of list[currNode]) {
+        if(!visited[nextNode]) {
+            dfs(max, visited, list, nextNode, currCost + cost);
+        }
     }
 }
-
-counting();
