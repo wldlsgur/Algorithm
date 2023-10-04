a277b5a30bf40351399ca4973eabbc401104276c
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-10-04 14:22:02 +0900

    [FIX] : 최소힙 리팩토링

--- 자료구조/힙(Heap)/MinHeap.js
+++ 자료구조/힙(Heap)/MinHeap.js
@@ -1,105 +1,83 @@
 class MinHeap {
-    heap = [];
-
     constructor() {
-        this.heap.push(null); // 편한 인덱스 계산을 위해 0번째 null 삽입
-    }
-
-    getParentIndex(index) {
-        return Math.floor(index / 2); // 부모 인덱스 계산
+        this.heap = [null];
     }
 
     getLeftChildIndex(index) {
-        return index * 2; // 왼쪽 자식 인덱스 계산
+        return index * 2;
     }
 
     getRightChildIndex(index) {
-        return index * 2 + 1; // 오른쪽 자식 인덱스 계산
+        return (index * 2) + 1;
+    }
+
+    getParentIndex(index) {
+        return Math.floor(index / 2);
+    }
+    
+    swap(index1, index2) {
+        const temp = this.heap[index1];
+
+        this.heap[index1] = this.heap[index2];
+        this.heap[index2] = temp;
     }
 
     isEmpty() {
-        return this.heap.length === 1; // 힙이 비었는지 확인
+        return !this.heap[1] ? true : false;
     }
 
-    printHeap() {
-        console.log(this.heap);
-    }
+    push(value) {
+        if(!value) {
+            return;
+        }
+        this.heap.push(value);
 
-    swapNode(parentIndex, childIndex) {
-        let temp = this.heap[parentIndex];
+        let currentIndex = this.heap.length - 1;
+        let parentIndex = this.getParentIndex(currentIndex);
         
-        this.heap[parentIndex] = this.heap[childIndex];
-        this.heap[childIndex] = temp;
+        while(parentIndex !== 0 && this.heap[parentIndex] > value) {
+            this.swap(currentIndex, parentIndex);
+
+            currentIndex = parentIndex;
+            parentIndex = this.getParentIndex(currentIndex);
+        }
     }
 
-    insertNode(node) {
-        if(!node) return;
+    pop() {
+        const result = this.heap[1];
+        let currentIndex = 1;
+        let leftIndex = 2;
+        let rightIndex = 3;
 
-        this.heap.push(node); // 힙 마지막에 노드 삽입
-
-        let currentIdx = this.heap.length - 1; // 현재 인덱스 타겟 마지막 인덱스
-
-        // 부모 노드가 더 크거나 힙에 노드가 2개 이상일때 반복
-        while(currentIdx > 1 && this.heap[this.getParentIndex(currentIdx)] > this.heap[currentIdx]) { 
-            this.swapNode(this.getParentIndex(currentIdx), currentIdx); // 부모 노드와 자식 노드 변경
-
-            currentIdx = this.getParentIndex(currentIdx);
-        }  
-    }
-
-    deleteNode() {
-        if(this.isEmpty()) return; // 힙이 비어있으면 바로 종료
-        if(this.heap.length === 2) return this.heap.pop(); // 힙의 개수가 1개이면 루트 노드 바로 반환
-
-        const deleteResult = this.heap[1]; // 루트 노드 가져오고
-        this.heap[1] = this.heap.pop(); // 마지막 노드 빼서 루트 노드로 이동
-
-        let currentIdx = 1;
-
-        while(true) {
-            const leftChildIdx = this.getLeftChildIndex(currentIdx);
-            const rightChildIdx = this.getRightChildIndex(currentIdx);
-
-            let smallIdx;
-
-            if(leftChildIdx >= this.heap.length) break; // 왼쪽 자식이 없으면 오른쪽 자식도 없기에 바로 종료
-
-            if(rightChildIdx < this.heap.length) { // 오른쪽 자식 까지 있으면 비교
-                smallIdx = this.heap[leftChildIdx] < this.heap[rightChildIdx] ? leftChildIdx : rightChildIdx;
-            } 
-            else { // 왼쪽 자식만 있는 경우 작은 값은 반드시 왼쪽
-                smallIdx = leftChildIdx;
-            }
-
-            if(this.heap[currentIdx] > this.heap[smallIdx]) { // 루트 노드와 작은 값의 자식 노드를 비교
-                this.swapNode(currentIdx, smallIdx);
-                currentIdx = smallIdx;
-            }
-            else { // 루트가 더 작으면 종료
-                break;
-            }
+        if(this.heap.length === 2) {
+            return this.heap.pop();
         }
 
-        return deleteResult;
+        this.heap[1] = this.heap.pop();
+
+        while(this.heap[currentIndex] > this.heap[leftIndex] || this.heap[currentIndex] > this.heap[rightIndex]) {
+            if(this.heap[leftIndex] < this.heap[rightIndex]) {
+                this.swap(currentIndex, leftIndex);
+                currentIndex = leftIndex;
+            }
+            else {
+                this.swap(currentIndex, rightIndex);
+                currentIndex = rightIndex;
+            }
+            leftIndex = this.getLeftChildIndex(currentIndex);
+            rightIndex = this.getRightChildIndex(currentIndex);
+        }
+
+        return result;
     }
 }
 
 const minHeap = new MinHeap();
 
-minHeap.insertNode(4);
-minHeap.insertNode(5);
-minHeap.insertNode(6);
-minHeap.insertNode(2);
-minHeap.insertNode(3);
-minHeap.insertNode(1);
-minHeap.insertNode(7);
+minHeap.push(5);
+minHeap.push(1);
+// minHeap.push(2);
 
-minHeap.printHeap();
-
-console.log(minHeap.deleteNode());
-console.log(minHeap.deleteNode());
-console.log(minHeap.deleteNode());
-console.log(minHeap.deleteNode());
-console.log(minHeap.deleteNode());
-console.log(minHeap.deleteNode());
-console.log(minHeap.deleteNode());
+console.log(minHeap.heap);
+console.log(minHeap.pop());
+console.log(minHeap.heap);
