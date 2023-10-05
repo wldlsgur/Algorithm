86f18b550b8dfe1571fd58c73eb21be3d21cb014
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-10-05 23:27:55 +0900

    [FIX] : 실버2 DFS와 BFS 주석 수정

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -13,18 +13,19 @@ rl.on("line", (input) => {
 
 function solution(N, M, V, G) {
     const list = {};
-    const visited = new Array(N + 1).fill(false);
-    const result = [[], []];
+    const visited = new Array(N + 1).fill(false); // dfs 방문 배열
+    const result = [[], []]; // [0] : dfs, 결과 [1] : bfs 결과 
 
+    // 1 ~ N까지 인접 리스트 빈 배열 초기화
     for(let i=1; i<=N ; i++) {
         list[i] = [];
     }
-
+    // 그래프 입력값으로 인접 리스트 양방향 그래프 값 삽입
     for(const [start, arrive] of G) {
         list[start].push(arrive);
         list[arrive].push(start);
     }
-
+    // 경로가 2개 이상이면 작은 숫자부터 가기위한 오름차순 정렬
     for(let i=1; i<=N ; i++) {
         list[i].sort((a, b) => a - b);
     }
@@ -39,7 +40,9 @@ function dfs(list, visited, N, start, result) {
     result[0].push(start);
     visited[start] = true;
 
+    // 현재 출발지 노드에서 갈 수 있는 노드들을 방문
     for(const nextNode of list[start]) {
+        // 방문하지 않은 노드들을 방문
         if(!visited[nextNode]) {
             dfs(list, visited, N, nextNode, result);
         }
@@ -47,17 +50,19 @@ function dfs(list, visited, N, start, result) {
 }
 
 function bfs(list, N, start, result) {
-    const queue = [start]
-    const visited = new Array(N + 1).fill(false);
+    const queue = [start];
+    const visited = new Array(N + 1).fill(false); // bfs 방문 배열
 
-    visited[start] = true;
+    visited[start] = true; // 현재 노드 방문 처리
 
     while(queue.length > 0) {
-        const currNode = queue.shift();
+        const currNode = queue.shift(); // 큐에서 노드를 꺼내고
 
-        result[1].push(currNode);
+        result[1].push(currNode); // 큐에서 꺼낸 순서대로 결과를 삽입
 
+        // 큐에서 꺼낸 현재 노드에서 갈 수 있는 노드들을 방문
         for(const nextNode of list[currNode]) {
+            // 방문하지 않은 노드들만 방문
             if(!visited[nextNode]) {
                 visited[nextNode] = true;
                 queue.push(nextNode);
