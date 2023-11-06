336a61b4e29dcaf2ba6bfbf482ed8e3a54ae2091
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2023-11-07 02:34:06 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1188 ms, Memory: 118196 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,55 +1,53 @@
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
-
 const inputs = [];
-let visited;
 
-rl.on("line", (input) => {
+rl.on('line', (input) => {
     inputs.push(input);
-}).on("close", () => {
-    const [N, M] = inputs[0].split(" ").map(Number);
-    const grapInfo = inputs.slice(1, inputs.length).map((value) => {
-        return value.split(" ").map(Number);
+}).on('close', () => {
+    const [N, M] = inputs[0].split(' ').map(Number);
+    const G = inputs.slice(1, inputs.length).map((value) => {
+        return value.split(' ').map(Number);
     });
 
-    solution(N, M, grapInfo);
+    solution(N, M, G);
 });
 
-function solution(N, M, grapInfo) { // N : 정점 개수 , M : 간선 개수
+function solution(N, M, G) {
+    // N : 정점 개수 , M : 간선 개수
     const list = createList(N);
-    visited = new Array(N + 1);
-
+    const visited = new Array(N + 1);
     let result = 0;
 
-    for(let i=0 ; i<M ; i++) {
-        const start = grapInfo[i][0];
-        const arrive = grapInfo[i][1];
+    for (let i = 0; i < M; i++) {
+        const start = G[i][0];
+        const arrive = G[i][1];
 
         insertList(list, start, arrive);
     }
 
-    for(let vertex in list) {
-        if(!visited[vertex]) {
-            result++;
-            bfs(list, vertex);
+    for (const vertex in list) {
+        if (!visited[vertex]) {
+            result += 1;
+            bfs(list, vertex, visited);
         }
     }
+
     console.log(result);
 }
 
-function bfs(list, start) {
-    const queue = [];
+function bfs(list, start, visited) {
+    const queue = [start];
 
-    queue.push(start);
     visited[start] = true;
 
-    while(queue.length > 0) {
+    while (queue.length > 0) {
         const vertex = queue.shift();
 
-        for(let i=0 ; i<list[vertex].length ; i++) {
+        for (let i = 0; i < list[vertex].length; i++) {
             const arrive = list[vertex][i];
 
-            if(!visited[arrive] && list[vertex]) {
+            if (!visited[arrive] && list[vertex]) {
                 visited[arrive] = true;
                 queue.push(arrive);
             }
@@ -60,7 +58,7 @@ function bfs(list, start) {
 function createList(N) {
     const list = {};
 
-    for(let i=1 ; i<=N ; i++){
+    for (let i = 1; i <= N; i++) {
         list[i] = [];
     }
 
