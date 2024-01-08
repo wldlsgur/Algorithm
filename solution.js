a6a913933fe9f6ed2efa18cbdb9abd9aef972d30
지인혁 <dhgg321@naver.com>
2024-01-09 03:17:03 +0900

    Fix: MinHeap 코드 리팩토링

--- solution.js
+++ solution.js
@@ -4,73 +4,73 @@ const inputs = [];
 rl.on('line', input => {
     inputs.push(input.trim());
 }).on('close', () => {
-    const N = Number(inputs.shift());
-    const parents = inputs.shift().split(' ').map(Number);
-    const deleteTarget = Number(inputs.shift());
+    const [N, M] = inputs.shift().split(' ').map(Number);
+    const [, ...trueNumbers] = inputs.shift().split(' ').map(Number); // 첫 번째 원소는 진실을 아는 사람의 수이므로 제외
+    const partyInfo = inputs.map(value =>
+        value.split(' ').slice(1).map(Number)
+    ); // 각 파티의 첫 번째 원소는 해당 파티의 사람 수이므로 제외
 
-    solution(N, parents, deleteTarget);
+    solution(N, M, trueNumbers, partyInfo);
 });
 
-function solution(N, parents, deleteTarget) {
-    const graph = {};
-    let startNode;
+function isCycle(parent, node1, node2) {
+    const parent1 = findParent(parent, node1);
+    const parent2 = findParent(parent, node2);
 
-    parents.forEach((parent, child) => {
-        if (parent === -1) {
-            startNode = child;
-        } else {
-            if (!graph[parent]) {
-                graph[parent] = [];
+    return parent1 === parent2;
+}
+
+function findParent(parent, node) {
+    if (parent[node] === node) return node;
+    return findParent(parent, parent[node]);
+}
+
+function union(parent, node1, node2) {
+    const parent1 = findParent(parent, node1);
+    const parent2 = findParent(parent, node2);
+
+    if (parent1 < parent2) parent[parent2] = parent1;
+    else parent[parent1] = parent2;
+}
+
+function solution(N, M, trueNumbers, partyInfo) {
+    // 유니온 파인드 집합 배열 생성
+    // 초기에는 자기 자신을 같은 집합으로 설정
+    const parent = Array.from({ length: N + 1 }, (_, i) => i);
+    let result = 0;
+
+    // 각 파티 마다 사람들을 순회
+    // 각 파티에 연결된 사람들은 하나의 집합으로 묶어준다.
+    // 한 사람이 다른 파티에 있다면 다른 파티도 같이 한 집합으로 묶어줘야한다.
+    // 비밀을 한 사람이 알면 그 전파되는 모든 파티사람들은 다 아니깐
+    for (let i = 0; i < M; i++) {
+        for (let j = 1; j < partyInfo[i].length; j++) {
+            const a = partyInfo[i][j - 1];
+            const b = partyInfo[i][j];
+
+            union(parent, a, b);
+        }
+    }
+
+    // 하나의 파티씩 순회
+    partyLoop: for (let i = 0; i < M; i++) {
+        // 파티에 한 사람씩 순회
+        for (let j = 0; j < partyInfo[i].length; j++) {
+            const people1 = partyInfo[i][j];
+
+            // 진실을 알고있는 사람들 목록을 순회
+            for (let z = 0; z < trueNumbers.length; z++) {
+                const people2 = trueNumbers[z];
+
+                // 진실을 알고있는 사람과 현재 파티에 참여중인 사람과 같은 집합이라면
+                // 그 파티에서는 진실을 이야기할 수 없다.
+                if (isCycle(parent, people1, people2)) {
+                    continue partyLoop;
+                }
             }
-            graph[parent].push(child);
         }
-    });
-
-    deleteNode(startNode, deleteTarget, graph);
-    console.log(countLeaf(startNode, graph));
-}
-
-function deleteNode(currentNode, deleteTarget, graph) {
-    if (currentNode === deleteTarget) {
-        removeNodes(currentNode, graph);
-
-        for (const parent in graph) {
-            graph[parent] = graph[parent].filter(
-                child => child !== deleteTarget
-            );
-        }
-
-        return;
+        result += 1;
     }
 
-    if (graph[currentNode]) {
-        for (const child of graph[currentNode]) {
-            deleteNode(child, deleteTarget, graph);
-        }
-    }
-}
-
-function removeNodes(node, graph) {
-    if (graph[node]) {
-        for (const child of graph[node]) {
-            removeNodes(child, graph);
-        }
-    }
-
-    delete graph[node];
-}
-
-function countLeaf(node, graph) {
-    let count = 0;
-
-    // 자식이 없는 리프 노드 경우
-    if (!graph[node]) {
-        return 1;
-    }
-
-    for (const child of graph[node]) {
-        count += countLeaf(child, graph);
-    }
-
-    return count;
+    console.log(result);
 }
--- 자료구조/힙(Heap)/MinHeap.js
+++ 자료구조/힙(Heap)/MinHeap.js
@@ -3,18 +3,10 @@ class MinHeap {
         this.heap = [null];
     }
 
-    getLeftChildIndex(index) {
-        return index * 2;
+    isEmpty() {
+        return this.heap[1] ? false : true;
     }
 
-    getRightChildIndex(index) {
-        return (index * 2) + 1;
-    }
-
-    getParentIndex(index) {
-        return Math.floor(index / 2);
-    }
-    
     swap(index1, index2) {
         const temp = this.heap[index1];
 
@@ -22,20 +14,25 @@ class MinHeap {
         this.heap[index2] = temp;
     }
 
-    isEmpty() {
-        return !this.heap[1] ? true : false;
+    getLeftChildIndex(index) {
+        return index * 2;
+    }
+
+    getRightChildIndex(index) {
+        return index * 2 + 1;
+    }
+
+    getParentIndex(index) {
+        return Math.floor(index / 2);
     }
 
     push(value) {
-        if(!value) {
-            return;
-        }
         this.heap.push(value);
 
         let currentIndex = this.heap.length - 1;
         let parentIndex = this.getParentIndex(currentIndex);
-        
-        while(parentIndex !== 0 && this.heap[parentIndex] > value) {
+
+        while (parentIndex !== 0 && this.heap[parentIndex] > value) {
             this.swap(currentIndex, parentIndex);
 
             currentIndex = parentIndex;
@@ -44,28 +41,41 @@ class MinHeap {
     }
 
     pop() {
-        const result = this.heap[1];
-        let currentIndex = 1;
-        let leftIndex = 2;
-        let rightIndex = 3;
-
-        if(this.heap.length === 2) {
-            return this.heap.pop();
+        if (this.isEmpty()) {
+            return 0;
         }
 
-        this.heap[1] = this.heap.pop();
+        const result = this.heap[1];
+        const lastValue = this.heap.pop();
 
-        while(this.heap[currentIndex] > this.heap[leftIndex] || this.heap[currentIndex] > this.heap[rightIndex]) {
-            if(this.heap[leftIndex] < this.heap[rightIndex]) {
-                this.swap(currentIndex, leftIndex);
-                currentIndex = leftIndex;
+        if (!this.isEmpty()) {
+            this.heap[1] = lastValue;
+
+            let currentIndex = 1;
+
+            while (true) {
+                const leftIndex = this.getLeftChildIndex(currentIndex);
+                const rightIndex = this.getRightChildIndex(currentIndex);
+
+                let smallerChildIndex = leftIndex;
+
+                if (
+                    this.heap[rightIndex] !== undefined &&
+                    this.heap[rightIndex] < this.heap[leftIndex]
+                ) {
+                    smallerChildIndex = rightIndex;
+                }
+
+                if (
+                    this.heap[smallerChildIndex] !== undefined &&
+                    this.heap[smallerChildIndex] < this.heap[currentIndex]
+                ) {
+                    this.swap(currentIndex, smallerChildIndex);
+                    currentIndex = smallerChildIndex;
+                } else {
+                    break;
+                }
             }
-            else {
-                this.swap(currentIndex, rightIndex);
-                currentIndex = rightIndex;
-            }
-            leftIndex = this.getLeftChildIndex(currentIndex);
-            rightIndex = this.getRightChildIndex(currentIndex);
         }
 
         return result;
