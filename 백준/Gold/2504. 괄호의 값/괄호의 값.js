d1218c222148cf78943435efb1830a08c7fa16c8
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-04-15 21:57:14 +0900

    [Gold V] Title: 괄호의 값, Time: 160 ms, Memory: 9720 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.js
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.js
@@ -1,56 +1,54 @@
-const input = [];
-const stack = [];
-
-const parseBracket = (bracket) => {
-  const bracketPair = bracket === ')' ? '(' : '[';
-  const lastStackElement = stack.pop();
-
-  if (lastStackElement === bracketPair) {
-    stack.push(bracketPair === '(' ? 2 : 3);
-    return true;
-  }
-  if (typeof lastStackElement === 'number') {
-    let temp = lastStackElement;
-    let t = stack.length;
-    while (t--) {
-      const lastStackElement = stack.pop();
-      if (lastStackElement === bracketPair) {
-        stack.push(temp * (bracketPair === '(' ? 2 : 3));
-        return true;
-      } else if (typeof lastStackElement === 'number') {
-        temp += lastStackElement;
-        continue;
-      } else {
-        return false;
-      }
-    }
-  }
-  return false;
-};
+const inputs = [];
 
 require('readline')
   .createInterface(process.stdin, process.stdout)
-  .on('line', function (line) {
-    input.push(line.trim());
+  .on('line', line => {
+    inputs.push(line.trim());
   })
-  .on('close', function () {
-    const bracketList = input[0].split('');
-    let i = 0;
-    do {
-      const bracket = bracketList[i];
-      if (bracket === '(' || bracket === '[') stack.push(bracket);
-      else {
-        if (!parseBracket(bracket)) {
-          console.log(0);
-          break;
-        }
-      }
-      i++;
+  .on('close', () => {
+    const str = inputs.shift();
 
-      if (i === bracketList.length) {
-        if (stack.length > 0 && !stack.find((e) => typeof e === 'string'))
-          console.log(stack.reduce((acc, curr) => acc + curr));
-        else console.log(0);
-      }
-    } while (i < bracketList.length);
+    console.log(solution(str));
+    process.exit();
   });
+
+function solution(str) {
+  const stack = [];
+
+  for (const char of [...str]) {
+    if (char === '(' || char === '[') {
+      stack.push(char);
+    } else {
+      const reverse = char === ')' ? '(' : '[';
+      const point = reverse === '(' ? 2 : 3;
+
+      if (stack.length === 0) { // 스택이 비어있는 경우 처리
+        return 0;
+      }
+
+      if (typeof stack[stack.length - 1] === 'number') {
+        let value = 0;
+        while (stack.length > 0 && typeof stack[stack.length - 1] === 'number') {
+          value += stack.pop();
+        }
+        if (stack.length === 0 || stack.pop() !== reverse) { // 적절한 여는 괄호가 없는 경우 처리
+          return 0;
+        }
+        stack.push(value * point);
+      } else {
+        if (stack.pop() !== reverse) { // 적절한 여는 괄호가 없는 경우 처리
+          return 0;
+        }
+        stack.push(point);
+      }
+    }
+  }
+
+  // 스택에 숫자만 남아있는지 확인하고, 그렇지 않다면 0 반환
+  if (stack.some(item => typeof item !== 'number')) {
+    return 0;
+  }
+
+  // 모든 검사를 통과했다면, 스택에 남은 숫자 합계 반환
+  return stack.reduce((acc, cur) => acc + cur, 0);
+}
