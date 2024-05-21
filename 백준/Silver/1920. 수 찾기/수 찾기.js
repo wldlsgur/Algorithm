cb9af15541b0a838333d114dddc555c2fc646ba3
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-05-22 01:47:56 +0900

    [Silver IV] Title: 수 찾기, Time: 368 ms, Memory: 40388 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,45 +1,49 @@
-const rl = require('readline').createInterface(process.stdin, process.stdout);
 const inputs = [];
 
-rl.on('line', input => {
-    inputs.push(input.trim());
-}).on('close', () => {
-    const N = Number(inputs.shift());
-    const arrayA = inputs.shift().split(' ').map(Number);
-    const M = Number(inputs.shift());
-    const arrayB = inputs.shift().split(' ').map(Number);
+require('readline')
+  .createInterface(process.stdin, process.stdout)
+  .on('line', line => {
+    inputs.push(line.trim());
+  })
+  .on('close', () => {
+    const N = inputs.shift();
+    const NNumbers = inputs.shift().split(' ').map(Number);
+    const M = inputs.shift();
+    const MNumbers = inputs.shift().split(' ').map(Number);
 
-    solution(N, M, arrayA, arrayB);
-});
+    console.log(solution(N, M, NNumbers, MNumbers));
+    process.exit();
+  });
 
-function solution(N, M, arrayA, arrayB) {
-    const sortedArrayA = arrayA.sort((a, b) => a - b);
-    const resultArray = [];
+function solution(N, M, NNumbers, MNumbers) {
+  const result = [];
+  NNumbers.sort((a, b) => a - b);
 
-    arrayB.forEach(number =>
-        resultArray.push(binarySearch(sortedArrayA, number))
-    );
-    console.log(resultArray.join('\n'));
+  MNumbers.forEach(number => {
+    result.push(binarySearch(NNumbers, number));
+  });
+
+  return result.join('\n');
 }
 
 function binarySearch(array, findValue) {
-    let left = 0;
-    let right = array.length - 1;
-    let mid = Math.floor((left + right) / 2);
+  let left = 0;
+  let right = array.length - 1;
+  let mid = Math.floor((left + right) / 2);
 
-    while (left <= right) {
-        if (array[mid] === findValue) {
-            return 1;
-        }
-
-        if (array[mid] < findValue) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-
-        mid = Math.floor((left + right) / 2);
+  while (left <= right) {
+    if (array[mid] === findValue) {
+      return 1;
     }
 
-    return 0;
+    if (array[mid] < findValue) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+
+    mid = Math.floor((left + right) / 2);
+  }
+
+  return 0;
 }
