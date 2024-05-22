4b2faba633704e25698328516840ce197e024d80
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-05-22 18:32:18 +0900

    [Gold IV] Title: 최단경로, Time: 1088 ms, Memory: 131864 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.js
+++ 백준/Gold/1753. 최단경로/최단경로.js
@@ -1,138 +1,142 @@
-const readline = require("readline");
-const rl = readline.createInterface(process.stdin, process.stdout);
 const inputs = [];
 
-rl.on("line", (input) => {
-    inputs.push(input.trim());
-}).on("close", () => {
-    const [V, E] = inputs[0].split(" ").map(Number);
-    const start = Number(inputs[1]);
-    const graphInfo = inputs.slice(2, inputs.length).map(value => value.split(" ").map(Number));
+require('readline')
+  .createInterface(process.stdin, process.stdout)
+  .on('line', line => {
+    inputs.push(line.trim());
+  })
+  .on('close', () => {
+    const [v, e] = inputs.shift().split(' ').map(Number);
+    const k = Number(inputs.shift());
+    const g = inputs.map(value => value.split(' ').map(Number));
 
-    solution(V, E, start, graphInfo);
-});
+    console.log(solution(v, e, k, g));
+    process.exit();
+  });
 
-function solution(V, E, start, graphInfo) {
-    const list = createList(V);
+function solution(v, e, k, g) {
+  const graph = {};
 
-    for(const uvw of graphInfo){
-        const [u, v, w] = uvw; // u : 시작점, v : 도착점, w : 가중치
-        
-        list[u].push([v, w]);
-    }
-    
-    const distance = dijkstra(list, start, V);
+  for (let i = 1; i <= v; i += 1) {
+    graph[i] = [];
+  }
 
-    distance.slice(1).map(value => value === Infinity ? console.log("INF") : console.log(value));
+  for (const [u, v, w] of g) {
+    graph[u].push([v, w]);
+  }
+
+  const result = dijkstra(graph, v, e, k);
+
+  return result
+    .slice(1)
+    .map(value => (value === Infinity ? 'INF' : value))
+    .join('\n');
 }
 
-function dijkstra(list, start, V) {
-    const priorityQueue = new MinHeap();
-    const visited = Array.from({ length: V + 1 }, () => false);
-    const distance = Array.from({ length: V + 1 }, () => Infinity);
+function dijkstra(graph, v, e, k) {
+  const minHeap = new MinHeap();
+  const visited = Array.from({ length: v + 1 }, () => false);
+  const distance = Array.from({ length: v + 1 }, () => Infinity);
 
-    distance[start] = 0; // 자기 자신 거리 0
-    priorityQueue.insert([start, 0]);
+  distance[k] = 0;
+  minHeap.push([k, 0]);
 
-    while(!priorityQueue.isEmpty()){
-        const [vertex, weight] = priorityQueue.getMinWeight();
+  while (!minHeap.isEmpty()) {
+    const [node] = minHeap.pop();
 
-        if (visited[vertex] || !vertex) continue;
-        else visited[vertex] = true;
-
-        for(let i=0 ; i<list[vertex].length ; i++) { // 연결된 정점 확인
-            const [arrive, weight] = list[vertex][i];
-
-            if(distance[arrive] > distance[vertex] + weight) { // 기존 최단경로가 현재 경로를 통해 갱신된 거리보다 크다면 새로 갱신
-              distance[arrive] = distance[vertex] + weight; // 연결된 경로의 최단경로를 현재 방문중인 정점의 최단경로 + 연결된 경로의 가중치
-              priorityQueue.insert([arrive, distance[arrive]]); // 우선순위에 정점과 최단경로를 삽입
-            }
-        }
+    if (visited[node]) {
+      continue;
     }
-    return distance;
-}
 
-function createList(V) {
-    const list = {};
+    visited[node] = true;
 
-    for(let i=1 ; i<=V ; i++) list[i] = [];
+    for (const [arrive, weight] of graph[node]) {
+      if (distance[arrive] > distance[node] + weight) {
+        distance[arrive] = distance[node] + weight;
+        minHeap.push([arrive, distance[arrive]]);
+      }
+    }
+  }
 
-    return list;
+  return distance;
 }
 
 class MinHeap {
-  heap = [];
-
   constructor() {
-    this.heap.push([null]);
+    this.heap = [null];
   }
 
+  getLeftIndex(index) {
+    return index * 2;
+  }
+  getRightIndex(index) {
+    return index * 2 + 1;
+  }
   getParentIndex(index) {
     return Math.floor(index / 2);
   }
-  getLeftChildIndex(index) {
-    return index * 2;
-  }
-  getRightChildIndex(index) {
-    return index * 2 + 1;
-  }
-
-  isEmpty() {
-    return this.heap[1] ? false : true;
-  }
-
   swap(index1, index2) {
-    let temp = this.heap[index1];
-    this.heap[index1] = this.heap[index2];
-    this.heap[index2] = temp;
+    return ([this.heap[index1], this.heap[index2]] = [
+      this.heap[index2],
+      this.heap[index1],
+    ]);
   }
-
-  insert(value) {
+  isEmpty() {
+    return this.heap.length === 1;
+  }
+  push(value) {
     this.heap.push(value);
 
     let currentIndex = this.heap.length - 1;
+    let parentIndex = this.getParentIndex(currentIndex);
 
     while (
-      currentIndex > 1 &&
-      this.heap[currentIndex][1] <
-        this.heap[this.getParentIndex(currentIndex)][1]
+      parentIndex > 0 &&
+      this.heap[parentIndex][1] > this.heap[currentIndex][1]
     ) {
-      this.swap(currentIndex, this.getParentIndex(currentIndex));
-      currentIndex = this.getParentIndex(currentIndex);
+      this.swap(currentIndex, parentIndex);
+      currentIndex = parentIndex;
+      parentIndex = this.getParentIndex(currentIndex);
     }
   }
-
-  getMinWeight() {
-    if (this.isEmpty()) return [null, null];
-
-    const [vertex, weight] = this.heap[1];
-
-    if (this.heap.length === 2) {
-      this.heap.pop();
-      return [vertex, weight];
+  pop() {
+    if (this.isEmpty()) {
+      return null;
     }
 
-    this.heap[1] = this.heap.pop();
+    const resultValue = this.heap[1];
+    const lastValue = this.heap.pop();
     let currentIndex = 1;
 
+    if (this.isEmpty()) {
+      return resultValue;
+    }
+
+    this.heap[1] = lastValue;
+
     while (true) {
-      const leftChildIndex = this.getLeftChildIndex(currentIndex);
-      const rightChildIndex = this.getRightChildIndex(currentIndex);
-      let smallerChildIndex;
+      const leftIndex = this.getLeftIndex(currentIndex);
+      const rightIndex = this.getRightIndex(currentIndex);
+      let smallerIndex = leftIndex;
 
-      if (leftChildIndex >= this.heap.length) break;
-
-      if (rightChildIndex < this.heap.length) {
-        smallerChildIndex = this.heap[leftChildIndex][1] < this.heap[rightChildIndex][1] ? leftChildIndex : rightChildIndex;
-      } else {
-        smallerChildIndex = leftChildIndex;
+      if (
+        this.heap[rightIndex] !== undefined &&
+        this.heap[rightIndex][1] < this.heap[leftIndex][1]
+      ) {
+        smallerIndex = rightIndex;
       }
 
-      if (this.heap[currentIndex][1] <= this.heap[smallerChildIndex][1]) break;
-
-      this.swap(currentIndex, smallerChildIndex);
-      currentIndex = smallerChildIndex;
+      if (
+        this.heap[smallerIndex] !== undefined &&
+        this.heap[currentIndex][1] > this.heap[smallerIndex][1]
+      ) {
+        this.swap(currentIndex, smallerIndex);
+        currentIndex = smallerIndex;
+      } else {
+        break;
+      }
     }
-    return [vertex, weight];
+
+    return resultValue;
   }
 }
