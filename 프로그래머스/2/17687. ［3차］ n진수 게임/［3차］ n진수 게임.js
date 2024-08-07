b5ce5ead0890cf9faaa9703f7174ee21e0d8decb
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-08-08 00:28:49 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.63 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -0,0 +1,19 @@
+function solution(n, t, m, p) {
+  const max = m * t;
+  let str = '';
+  let result = '';
+
+  for (let i = 0; str.length < max; i += 1) {
+    str += i.toString(n).toUpperCase();
+  }
+  let cnt = 0;
+
+  while (result.length < t) {
+    const sliceStr = str.substring(cnt, cnt + m);
+
+    result += sliceStr[p - 1];
+    cnt += m;
+  }
+
+  return result;
+}
