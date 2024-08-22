7a00873f5be46aceb8461689c90b4dc4b853e459
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-08-23 00:43:10 +0900

    [level 2] Title: 땅따먹기, Time: 48.85 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,12 @@
+function solution(land) {
+  const dp = [...land[0]];
+
+  for (let i = 0; i < land.length - 1; i += 1) {
+    land[i + 1][0] += Math.max(land[i][1], land[i][2], land[i][3]);
+    land[i + 1][1] += Math.max(land[i][0], land[i][2], land[i][3]);
+    land[i + 1][2] += Math.max(land[i][0], land[i][1], land[i][3]);
+    land[i + 1][3] += Math.max(land[i][0], land[i][1], land[i][2]);
+  }
+
+  return Math.max(...land[land.length - 1]);
+}
