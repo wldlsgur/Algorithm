f8cb8781eb2481a07c6e9dd1203588ba18d7ee85
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-09-27 20:59:42 +0900

    [level 2] Title: 택배상자, Time: 142.05 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -0,0 +1,15 @@
+function solution(order) {
+  let result = 0;
+  const stack = [];
+
+  for (let i = 1; i <= order.length; i += 1) {
+    stack.push(i);
+
+    while (stack.length !== 0 && stack.at(-1) === order[result]) {
+      stack.pop();
+      result++;
+    }
+  }
+
+  return result;
+}
