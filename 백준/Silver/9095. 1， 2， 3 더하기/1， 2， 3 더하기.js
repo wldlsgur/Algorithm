5edc159dabd117f4757744f39f69bb771a0fcc86
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-10-25 02:05:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 9452 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,34 +1,34 @@
-const readline = require("readline");
-const rl = readline.createInterface(process.stdin, process.stdout);
 const inputs = [];
 
-rl.on("line", (input) => {
-    inputs.push(input.trim());
-}).on("close" , () => {
-    const T = Number(inputs.shift());
+require('readline')
+  .createInterface(process.stdin, process.stdout)
+  .on('line', (line) => {
+    inputs.push(line.trim());
+  })
+  .on('close', () => {
+    const t = Number(inputs.shift());
+    const numbers = inputs.map(Number);
 
-    for(let tc=0 ; tc<T ; tc++) {
-        const N = Number(inputs.shift());
+    console.log(solution(t, numbers));
+    process.exit();
+  });
 
-        solution(N);
+function solution(t, numbers) {
+  const result = [];
+
+  numbers.forEach((number) => {
+    const dp = new Array(number + 1).fill(0);
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for (let i = 4; i <= number; i += 1) {
+      dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
     }
-})
 
-function solution(N) {
-    const dp = new Array(N + 1).fill(0);
-    
-    dp[1] = 1; // 1일때 (1) 1개
-    dp[2] = 2; // 2일때 (1 + 1, 2) 2개
-    dp[3] = 4; // 3일때 (1 + 1 + 1, 2 + 1, 1 + 2, 3) 4개
+    result.push(dp[number]);
+  });
 
-    // 4일때 
-    // 1의 (1 + 3) 1개 + 
-    // 2의 (1 + 1 + 2, 2 + 2) 2개 +
-    // 3의 (1 + 1 + 1 + 1, 2 + 1 + 1, 1 + 2 + 1, 3 + 1) 4개 
-
-    for(let i=4 ; i<=N ; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-    }
-
-    console.log(dp[N]);
+  return result.join('\n');
 }
