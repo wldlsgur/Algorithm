3f3ea24ae3ba14ebba20ad9631de7ee17cf143bf
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2025-06-29 22:31:08 +0900

    [level 2] Title: [3차] n진수 게임, Time: 8.28 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,19 +1,17 @@
 function solution(n, t, m, p) {
-  const max = m * t;
-  let str = '';
-  let result = '';
+  const maxLength = m * t; // 전체 인원이 말하는 최대 문자 수 (튜브 순서 포함해서 t개 확보하기 위해)
+  let allSequence = '';    // 0부터 n진수로 변환된 모든 숫자를 이어붙일 문자열
+  let result = '';         // 튜브가 말해야 하는 문자들을 저장할 결과 문자열
 
-  for (let i = 0; str.length < max; i += 1) {
-    str += i.toString(n).toUpperCase();
+  // 0부터 시작해서 n진수로 바꾸고 전부 이어붙임 (최소 maxLength 만큼 확보될 때까지)
+  for (let i = 0; allSequence.length < maxLength; i++) {
+    allSequence += i.toString(n).toUpperCase(); // toUpperCase는 A~F 대문자 대응
   }
-    
-  let cnt = 0;
 
-  while (result.length < t) {
-    const sliceStr = str.substring(cnt, cnt + m);
-
-    result += sliceStr[p - 1];
-    cnt += m;
+  // 튜브가 말하는 순서(p)에 맞는 문자를 m 간격으로 뽑아 t개 저장
+  for (let i = 0; result.length < t; i++) {
+    const index = i * m + (p - 1); // p번째 사람의 순서는 (p - 1)번 인덱스
+    result += allSequence[index];
   }
 
   return result;
